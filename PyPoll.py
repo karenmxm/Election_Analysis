da34a9e40de0ebabdf470cb87094b5207faa53c0
mengxiaomeng01 <mengxiao23@gmail.com>
2020-04-17 19:02:38 -0500

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,16 @@
+import csv
+import os
+# # Assign a variable for the file to load and the path.
+file_to_load = os.path.join('Resources',"election_results.csv")
+
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Open the election results and read the file.
+with open(file_to_load,newline="") as election_data:
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data,delimiter=',')
+
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
